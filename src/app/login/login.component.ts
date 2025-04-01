76a6bbbe19cee98f393f7f79869704acdb90c4e5
Paul Brenker <paul.brenker@smail.inf.h-brs.de>
2025-04-01 23:30:30 +0300

    prepare login component for link to signup

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import axios from 'axios';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
   username: string = '';
   password: string = '';
+  errorMessage: string | null = null;
 
   constructor(private router: Router) {}
   ngOnInit(): void {
@@ -36,7 +38,7 @@ export class LoginComponent {
       console.log('Login successful, token:', response.data.token);
       this.router.navigate(['/']);
     } catch (error) {
-      console.error('Login failed:', error);
+      this.errorMessage = 'Invalid username or password';
     }
   }
 }
