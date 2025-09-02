84eb9ef167f15f676d691b7f96f3346a66ad4104
Paul Brenker <paul.brenker@smail.inf.h-brs.de>
2025-09-02 16:54:34 +0300

    basic content component setup and system theme ui

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ContentComponent } from './content/content.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, ContentComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
